5d01115db28f0d9f2f61ad4d3b389b30cb37e125
Jonas <jonasdj@protonmail.com>
2020-11-30 17:05:01 +0100

    Vi brændte tingene ned og byggede det op fra ny

--- js/js.js
+++ js/js.js
@@ -2,11 +2,16 @@
  * file: js.js
  * purpose: interactivity
  **/
-console.log('JavaScript from js/js.js: up and running!');
+console.log("JavaScript from js/js.js: up and running!");
 
 // Tjek at DOM er indlæst
-window.onload(function () {
-    // ... your code here ...
-    
-    // ... end ...
+$(document).ready(function () {
+  // ... your code here ...
+  $("#test-menu").hide();
+
+  $("#burger-icon").click(function () {
+    $("#test-menu").toggle();
+  });
+
+  // ... end ...
 });
---
+++ js/template.js
@@ -0,0 +1,20 @@
+/**
+ * file: js.js
+ * purpose: interactivity
+ **/
+console.log("JavaScript from js/js.js: up and running!");
+
+// Tjek at DOM er indlæst
+$(document).ready(function () {
+  // ... your code here ...
+$("#test-menu").hide();
+
+
+  $("#burger-icon").click(function () {
+    $("#test-menu").toggle();
+  });
+
+
+
+  // ... end ...
+});
